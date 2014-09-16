1d45c9f1709794cb238d64bac49c73f237de4937
sfaenza <cinzia.faenza@gmail.com>
2014-09-16 12:04:04 -0700

    update environment deploy scripts

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -16,7 +16,7 @@
 # used to set extended properties on the server.
 
 # server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
-server 'dash-dev.cdlib.org', user: 'dash', roles: %w{web app}
+server 'dash.cdlib.org', user: 'dash', roles: %w{web app}
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/apps/dash/apache/htdocs/dash.cdlib.org'
