29710f931a1edd22da4a4ff175f86f28a0402dbd
Scott Fisher <scott.fisher@ucop.edu>
2016-03-31 15:13:10 -0700

    Changing deployment targets in capistrano files for new AWS servers.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -16,7 +16,7 @@
 # used to set extended properties on the server.
 
 # server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
-server 'dash-stg.cdlib.org', user: 'dash', roles: %w{web app}
+server 'uc3-dash-stg.cdlib.org', user: 'dash', roles: %w{web app}
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/apps/dash/apps/apache/htdocs/dash-stg.cdlib.org'
